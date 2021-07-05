8e2304fb0b7296116c35b41e181ed034b22ae05c
Subham raj <45128519+Shubhamr837@users.noreply.github.com>
2021-07-05 11:31:28 +0530

    Update UserController.java

--- src/main/java/com/debugshark/userservice/controllers/UserController.java
+++ src/main/java/com/debugshark/userservice/controllers/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.json.JSONObject;
 
-@RestController
+@RestController(value="user")
 public class UserController {
 @Autowired
    private UserDaoImpl userDao;
